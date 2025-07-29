4c2d114b4f43e2784e8315a185f311bce7991a7d
bhavani-mudrakola <49580249+bhavani-mudrakola@users.noreply.github.com>
2025-07-29 09:38:44 +0530

    seventh commit

--- SampleJava.java
+++ SampleJava.java
@@ -4,5 +4,6 @@ class SampleJava{
 		System.out.println("Hello World!!");
 		System.out.println("Welcome Jenkins!!");
 		System.out.println("Java Program in Jenkins!!");
+		System.out.println("SCM Polling to monitor change commits in git hub!!");
 	}
 }
